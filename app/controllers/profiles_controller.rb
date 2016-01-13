d049eb483ac744262d18c1b72c1abf6f06cb2816
Chai Wei Jun <chaiweijun@Chais-Mac.local>
2016-01-13 16:40:09 +0800

    creates new instance of classes from json information

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
 	def new
+		Profile.last.destroy if Profile.last
 	end
 
 	def create
@@ -7,13 +8,48 @@ class ProfilesController < ApplicationController
 		if is_json?(file.tempfile)
 			json = JSON.parse(file.read)
 			@profile = Profile.new
-			
-			redirect_to "/profile"
+			@profile.name = json["name"]
+    	@profile.phone = json["phone"]
+    	@profile.email = json["email"]
+    	@profile.street = json["address"]["street"]
+    	@profile.city = json["address"]["city"]
+    	@profile.postcode = json["address"]["postcode"]
+    	@profile.state = json["address"]["state"]
+    	@profile.image = json["image"]
+    	@profile.about = json["about"]
+    	@profile.languages = json["languages"]
+
+    	if @profile.save
+
+    		json["education"].each do |edu|
+    			e = Education.new(edu)
+    			e.profile_id = @profile.id 
+    			e.save
+    		end
+
+    		json["workExperience"].each do |exp|
+    			w = WorkExperience.new(exp)
+    			w.profile_id = @profile.id 
+    			w.save
+    		end
+
+    		json["projects"].each do |pro|
+    			p = Project.new(pro)
+    			p.profile_id = @profile.id 
+    			p.save
+    		end
+
+				redirect_to "/profile"
+
+			else
+				flash[:alert] = "Please upload a properly formatted resume JSON."
+				render "new"
+			end
+
 		else
 			flash[:alert] = "Please upload a valid JSON file."
 			render "new"
 		end
-
 	end
 
 	def show
